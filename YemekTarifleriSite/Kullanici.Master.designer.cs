287e4d794a99f3b4ee6bc43f347d50daf6cbe827
Samet Ünverdi <sametunv@gmail.com>
2024-03-26 02:46:02 +0300

    video 7 bitti.

--- YemekTarifleriSite/Kullanici.Master.designer.cs
+++ YemekTarifleriSite/Kullanici.Master.designer.cs
@@ -33,6 +33,15 @@ namespace YemekTarifleriSite
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// DataList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList1;
+
+        /// <summary>
         /// ContentPlaceHolder2 control.
         /// </summary>
         /// <remarks>
